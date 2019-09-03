62c3b68e9fcf46aa7ec89c4f809473ec831840fa
blotteracid <klubniiikaaa@mail.ru>
2019-09-03 20:18:39 +0400

    Add Prettier

--- scripts.js
+++ scripts.js
@@ -1,32 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("#form");
-    const submitButton = document.querySelector(".form-button");
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('#form')
+  const submitButton = document.querySelector('.form-button')
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        if (submitButton.textContent == "Logout") location.reload();
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    if (submitButton.textContent == 'Logout') location.reload()
 
-        fetch("https://us-central1-mercdev-academy.cloudfunctions.net/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email: document.querySelector(".form-input[name='email']").value,
-                password: document.querySelector(".form-input[name='password']").value
-            })
-        }).then(function(res) {
-            return res.json();
-        }).then(function(result) { 
-            if (result.error) {
-                form.classList.add("invalid");
-            } else {
-                form.classList.remove("invalid");
-                form.classList.add("success");
-                document.querySelector(".form-title").textContent = result.name;
-                document.querySelector(".avatar").src = result.photoUrl;
-                submitButton.textContent = "Logout";
-            }
-        });
+    fetch('https://us-central1-mercdev-academy.cloudfunctions.net/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: document.querySelector(".form-input[name='email']").value,
+        password: document.querySelector(".form-input[name='password']").value,
+      }),
     })
-});
+      .then(function(res) {
+        return res.json()
+      })
+      .then(function(result) {
+        console.log(result)
+        if (result.error) {
+          form.classList.add('invalid')
+        } else {
+          form.classList.remove('invalid')
+          form.classList.add('success')
+          document.querySelector('.form-title').textContent = result.name
+          document.querySelector('.avatar').src = result.photoUrl
+          submitButton.textContent = 'Logout'
+        }
+      })
+  })
+})
