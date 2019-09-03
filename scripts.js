24caddbd2fbe4e5a4a7db95c51c831926b2b7b90
blotteracid <klubniiikaaa@mail.ru>
2019-09-03 21:44:50 +0400

    Fix JS

--- scripts.js
+++ scripts.js
@@ -2,34 +2,54 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('#form')
   const submitButton = document.querySelector('.form__button')
 
-  form.addEventListener('submit', function(event) {
-    event.preventDefault()
-    if (submitButton.textContent == 'Logout') location.reload()
+  function logout() {
+    location.reload()
+  }
 
+  function formError(error) {
+    form.classList.add('form_invalid')
+  }
+
+  function formSuccess(name, photo) {
+    form.classList.remove('form_invalid')
+    form.classList.add('form_success')
+    document.querySelector('.form__title').textContent = name
+    document.querySelector('.avatar').src = photo
+    submitButton.textContent = 'Logout'
+  }
+
+  function login(email, password) {
     fetch('https://us-central1-mercdev-academy.cloudfunctions.net/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        email: document.querySelector(".form__input[name='email']").value,
-        password: document.querySelector(".form__input[name='password']").value,
+        email: email,
+        password: password,
       }),
     })
       .then(function(res) {
         return res.json()
       })
       .then(function(result) {
-        console.log(result)
         if (result.error) {
-          form.classList.add('form_invalid')
+          formError(result.error)
         } else {
-          form.classList.remove('form_invalid')
-          form.classList.add('form_success')
-          document.querySelector('.form__title').textContent = result.name
-          document.querySelector('.avatar').src = result.photoUrl
-          submitButton.textContent = 'Logout'
+          formSuccess(result.name, result.photoUrl)
         }
       })
+  }
+
+  form.addEventListener('submit', function(event) {
+    const target = event.target
+
+    event.preventDefault()
+    if (submitButton.textContent == 'Logout') logout()
+
+    const email = target.querySelector(".form__input[name='email']").value
+    const password = target.querySelector(".form__input[name='password']").value
+
+    login(email, password)
   })
 })
